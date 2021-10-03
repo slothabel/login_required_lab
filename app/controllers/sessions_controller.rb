1734464cf1e8680b7a47cf3878f1aeed6703ccb6
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-10-03 18:42:58 -0400

    commit tree a2376673a2529a1f968918fab76448678699f051

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,18 @@
 class SessionsController < ApplicationController
+
     def new
     end
 
     def create
+        if params[:name] && !params[:name].empty?
+            session[:name] = params[:name]
+        else
+            require_login
+        end
     end
     
     def destroy
+        session.delete :name
+        redirect_to new_session_path
     end
 end
